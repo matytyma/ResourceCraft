5ede2211d1effb3f0d8f17ca74f98f075cd1365c
matytyma <matytyma22@gmail.com>
2024-10-30 21:49:02 +0100

    Add experimental texture animations

--- src/main/kotlin/dev/matytyma/opencraft/OpenCraft.kt
+++ src/main/kotlin/dev/matytyma/opencraft/OpenCraft.kt
@@ -2,8 +2,11 @@ package dev.matytyma.opencraft
 
 import dev.matytyma.opencraft.pack.NamespacedKey
 import dev.matytyma.opencraft.pack.PackMeta
+import dev.matytyma.opencraft.pack.blockstate.BlockState
+import dev.matytyma.opencraft.pack.blockstate.BlockStateVariant
 import dev.matytyma.opencraft.pack.model.*
 import dev.matytyma.opencraft.version.ServerVersion
+import kotlinx.serialization.Serializable
 import kotlinx.serialization.encodeToString
 import kotlinx.serialization.json.Json
 import org.bukkit.event.Listener
@@ -19,14 +22,22 @@ class OpenCraft : JavaPlugin(), Listener {
             val packMeta = PackMeta("A cool pack", ServerVersion.fromVersionString(server.minecraftVersion).packFormat)
             zos.addJsonFile("pack.mcmeta", packMeta)
             File(dataFolder, "pack.png").let {
-                if (it.exists()) {
-                    zos.addFile("pack.png", File(dataFolder, "pack.png").readBytes())
-                    zos.addFile("assets/minecraft/textures/item/matytyma.png", File(dataFolder, "pack.png").readBytes())
-                }
+                if (!it.exists()) return@let
+                zos.addFile("pack.png", File(dataFolder, "pack.png").readBytes())
+                zos.addFile("assets/minecraft/textures/item/matytyma.png", it.readBytes())
             }
+
+            File(dataFolder, "rick.png").let {
+                if (!it.exists()) return@let
+                zos.addFile("assets/minecraft/textures/block/rick.png", it.readBytes())
+            }
+
             zos.addJsonFile(
                 "assets/minecraft/models/item/matytyma.json",
-                ItemModel(NamespacedKey.minecraft("item/diamond"), mapOf("layer0" to NamespacedKey.minecraft("item/matytyma")))
+                ItemModel(
+                    NamespacedKey.minecraft("item/diamond"),
+                    mapOf("layer0" to NamespacedKey.minecraft("item/matytyma"))
+                )
             )
 
             zos.addJsonFile(
@@ -36,12 +47,29 @@ class OpenCraft : JavaPlugin(), Listener {
                     mapOf("layer0" to NamespacedKey.minecraft("item/diamond")),
                     listOf(
                         ModelOverride(
-                            Predicate(1),
+                            ItemPredicate(1),
                             NamespacedKey.minecraft("item/matytyma")
                         )
                     )
                 )
             )
+
+            zos.addJsonFile(
+                "assets/minecraft/blockstates/note_block.json",
+                BlockState(mapOf("instrument=didgeridoo" to BlockStateVariant(NamespacedKey.minecraft("block/rick"))))
+            )
+
+            zos.addJsonFile(
+                "assets/minecraft/models/block/rick.json",
+                BlockModel(
+                    NamespacedKey.minecraft("block/decorated_pot"),
+                    NamespacedKey.minecraft("block/rick").let {
+                        mapOf("particle" to it, "all" to it, "texture" to it)
+                    }
+                )
+            )
+
+            zos.addJsonFile("assets/minecraft/textures/block/rick.png.mcmeta", TextureMeta(TextureAnimation(2)))
         }
         file.copyTo(File(System.getenv("PACK_PATH")), true)
     }
@@ -57,3 +85,14 @@ private fun ZipOutputStream.addFile(name: String, content: String) = addFile(nam
 
 private inline fun <reified T> ZipOutputStream.addJsonFile(name: String, content: T) =
     addFile(name, Json.encodeToString<T>(content))
+
+
+@Serializable
+data class TextureMeta(
+    val animation: TextureAnimation,
+)
+
+@Serializable
+data class TextureAnimation(
+    val frametime: Int,
+)
